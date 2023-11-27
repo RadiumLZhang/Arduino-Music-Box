79f25b4eb67d728f4e3ac446bc4c9feb7f229ead
Radium Zhang <Radium0729@gmail.com>
2023-11-27 09:52:16 -0500

    feat(Arduino-Connection): add wireless setting and connection

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,9 +1,98 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+const char* ssid     = "I believe Wi can Fi";
+const char* password = "Jirachi666";
 
+// Buzzer pin
+const int buzzerPin = 8; // Change this to a suitable GPIO pin
+
+WiFiServer server(80);
+
+
+#define NOTE_D0 -1
+#define NOTE_D1 294
+#define NOTE_D2 330
+#define NOTE_D3 350
+#define NOTE_D4 393
+#define NOTE_D5 441
+#define NOTE_D6 495
+#define NOTE_D7 556
+
+#define NOTE_DL1 147
+#define NOTE_DL2 165
+#define NOTE_DL3 175
+#define NOTE_DL4 196
+#define NOTE_DL5 221
+#define NOTE_DL6 248
+#define NOTE_DL7 278
+
+#define NOTE_DH1 589
+#define NOTE_DH2 661
+#define NOTE_DH3 700
+#define NOTE_DH4 786
+#define NOTE_DH5 882
+#define NOTE_DH6 990
+#define NOTE_DH7 112
+
+int tune[] = {
+  NOTE_D0, NOTE_D0, NOTE_D0, NOTE_D6, NOTE_D7, NOTE_DH1, NOTE_D7, NOTE_DH1, NOTE_DH3, NOTE_D7, NOTE_D7, NOTE_D7, NOTE_D3, NOTE_D3,
+  NOTE_D6, NOTE_D5, NOTE_D6, NOTE_DH1, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D3, NOTE_D4, NOTE_D3, NOTE_D4, NOTE_DH1,
+  NOTE_D3, NOTE_D3, NOTE_D0, NOTE_DH1, NOTE_DH1, NOTE_DH1, NOTE_D7, NOTE_D4, NOTE_D4, NOTE_D7, NOTE_D7, NOTE_D7, NOTE_D0, NOTE_D6, NOTE_D7,
+  NOTE_DH1, NOTE_D7, NOTE_DH1, NOTE_DH3, NOTE_D7, NOTE_D7, NOTE_D7, NOTE_D3, NOTE_D3, NOTE_D6, NOTE_D5, NOTE_D6, NOTE_DH1,
+  NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D2, NOTE_D3, NOTE_D4, NOTE_DH1, NOTE_D7, NOTE_D7, NOTE_DH1, NOTE_DH1, NOTE_DH2, NOTE_DH2, NOTE_DH3, NOTE_DH1, NOTE_DH1, NOTE_DH1,
+  NOTE_DH1, NOTE_D7, NOTE_D6, NOTE_D6, NOTE_D7, NOTE_D5, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_DH1, NOTE_DH2, NOTE_DH3, NOTE_DH2, NOTE_DH3, NOTE_DH5,
+  NOTE_DH2, NOTE_DH2, NOTE_DH2, NOTE_D5, NOTE_D5, NOTE_DH1, NOTE_D7, NOTE_DH1, NOTE_DH3, NOTE_DH3, NOTE_DH3, NOTE_DH3, NOTE_DH3,
+  NOTE_D6, NOTE_D7, NOTE_DH1, NOTE_D7, NOTE_DH2, NOTE_DH2, NOTE_DH1, NOTE_D5, NOTE_D5, NOTE_D5, NOTE_DH4, NOTE_DH3, NOTE_DH2, NOTE_DH1,
+  NOTE_DH3, NOTE_DH3, NOTE_DH3, NOTE_DH3, NOTE_DH6, NOTE_DH6, NOTE_DH5, NOTE_DH5, NOTE_DH3, NOTE_DH2, NOTE_DH1, NOTE_DH1, NOTE_D0, NOTE_DH1,
+  NOTE_DH2, NOTE_DH1, NOTE_DH2, NOTE_DH2, NOTE_DH5, NOTE_DH3, NOTE_DH3, NOTE_DH3, NOTE_DH3, NOTE_DH6, NOTE_DH6, NOTE_DH5, NOTE_DH5,
+  NOTE_DH3, NOTE_DH2, NOTE_DH1, NOTE_DH1, NOTE_D0, NOTE_DH1, NOTE_DH2, NOTE_DH1, NOTE_DH2, NOTE_DH2, NOTE_D7, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_D6, NOTE_D7
+};
+
+float duration[] =
+
+  {
+    1, 1, 1, 0.5, 0.5, 1 + 0.5, 0.5, 1, 1, 1, 1, 1, 0.5, 0.5,
+    1 + 0.5, 0.5, 1, 1, 1, 1, 1, 1, 1 + 0.5, 0.5, 1, 1,
+    1, 1, 0.5, 0.5, 0.5, 0.5, 1 + 0.5, 0.5, 1, 1, 1, 1, 1, 0.5, 0.5,
+    1 + 0.5, 0.5, 1, 1, 1, 1, 1, 0.5, 0.5, 1 + 0.5, 0.5, 1, 1,
+    1, 1, 1, 0.5, 0.5, 1, 0.5, 0.25, 0.25, 0.25, 0.5, 0.5, 0.5, 0.5, 0.25, 0.5, 1,
+    0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1, 1, 0.5, 0.5, 1 + 0.5, 0.5, 1, 1,
+    1, 1, 1, 0.5, 0.5, 1.5, 0.5, 1, 1, 1, 1, 1, 1,
+    0.5, 0.5, 1, 1, 0.5, 0.5, 1.5, 0.25, 0.5, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 0.5, 0.5, 1, 1, 0.5, 0.5,
+    1, 0.5, 0.5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0.5, 0.5, 1, 1, 0.5, 0.5, 1, 0.5, 0.25, 0.5, 1, 1, 1, 1, 0.5, 0.5
+  };
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(115200);
+
+  // Setup WiFi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Check for incoming data or commands here
+  // ...
 
+  // Example: Play a tune
+  playTune(tune, duration, sizeof(tune)/sizeof(tune[0]));
+  delay(10000); // Delay between playbacks
+}
+
+void playTune(int* tune, float* duration, int length) {
+  for (int i = 0; i < length; ++i) {
+    if (tune[i] == NOTE_D0) {
+      noTone(buzzerPin); // Stop any previous tone
+    } else {
+      tone(buzzerPin, tune[i], (int)(duration[i] * 1000)); // Play tone
+    }
+    delay((int)(duration[i] * 1000)); // Duration of the note
+  }
+  noTone(buzzerPin); // Stop tone at the end
 }
